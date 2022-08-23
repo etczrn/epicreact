eb106d23f387f42080aef2aab3625d323180619c
etczrn <etczrn@gmail.com>
2022-08-23 14:51:07 +0900

    Accept a Size Prop to Encapsulate Styling #5

--- react-fundamentals/src/exercise/05.js
+++ react-fundamentals/src/exercise/05.js
@@ -4,27 +4,44 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 🐨 add a className prop to each div and apply the correct class names
-// based on the text content
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-// 💰 each of the elements should have the "box" className applied
+function Box({style, size, className, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ''
 
-// 🐨 add a style prop to each div so their background color
-// matches what the text says it should be
-// 🐨 also use the style prop to make the font italic
-// 💰 Here are available style attributes: backgroundColor, fontStyle
+  return (
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = (
+  <Box id="small-box" size="small" style={{backgroundColor: 'lightBlue'}}>
+    small lightblue box
+  </Box>
+)
+
+const mediumBox = (
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+
+const largeBox = (
+  <Box size="large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
 
 function App() {
   return (
-    <div>
+    <>
       {smallBox}
       {mediumBox}
       {largeBox}
-    </div>
+      <Box>sizeless box</Box>
+    </>
   )
 }
 
