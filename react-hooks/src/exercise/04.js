cfec618afe7445f087bdd092e84c0e56cc7bda0d
etczrn <etczrn@gmail.com>
2022-09-01 15:28:59 +0900

    Use useLocalStorageState Custom Hook #12

--- react-hooks/src/exercise/04.js
+++ react-hooks/src/exercise/04.js
@@ -2,11 +2,12 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
 function Board() {
-  const [squares, setSquares] = React.useState(
-    () =>
-      JSON.parse(window.localStorage.getItem('squares')) ?? Array(9).fill(null),
+  const [squares, setSquares] = useLocalStorageState(
+    'squares',
+    Array(9).fill(null),
   )
 
   const nextValue = calculateNextValue(squares)
