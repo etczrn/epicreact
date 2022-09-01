1a367d730428050e561bb5de09b86eb1ec5c65ec
etczrn <etczrn@gmail.com>
2022-09-01 18:13:51 +0900

    Store the state in an object #14

--- react-hooks/src/exercise/06.js
+++ react-hooks/src/exercise/06.js
@@ -10,28 +10,28 @@ import {
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  const [status, setStatus] = React.useState('idle')
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error, setError] = React.useState(null)
+  const [state, setState] = React.useState({
+    status: 'idle',
+    pokemon: null,
+    error: null,
+  })
+  const {status, pokemon, error} = state
 
   React.useEffect(() => {
     if (!pokemonName) return
 
-    setStatus('pending')
+    setState({status: 'pending'})
 
     fetchPokemon(pokemonName).then(
-      pokemonData => {
-        setPokemon(pokemonData)
-        setStatus('resolved')
+      pokemon => {
+        setState({status: 'resolved', pokemon})
       },
       error => {
-        setError(error)
-        setStatus('rejected')
+        setState({status: 'rejected', error})
       },
     )
   }, [pokemonName])
 
-  // * Render different UI based on that status variable
   if (status === 'idle') {
     return 'Submit a pokemon'
   } else if (status === 'pending') {
