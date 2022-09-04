e61671a892f9a255175fa215a7b8b74369b881b7
etczrn <etczrn@gmail.com>
2022-09-04 20:43:28 +0900

    Support function API and object API #15

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -3,16 +3,18 @@
 
 import * as React from 'react'
 
-function countReducer(state, action) {
-  return {...state, ...action}
-}
+const countReducer = (state, action) => ({
+  ...state,
+  ...(typeof action === 'function' ? action(state) : action),
+})
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
