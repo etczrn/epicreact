a73b08b3ff66af02691b88f0996036fed71baeb6
etczrn <etczrn@gmail.com>
2022-09-08 15:49:48 +0900

    Create a LoginForm component #27

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -4,10 +4,43 @@ import {createRoot} from 'react-dom/client'
 import Dialog from '@reach/dialog'
 import {Logo} from 'components/logo'
 
+const LoginForm = ({onSubmit, buttonText}) => {
+  const handleSubmit = e => {
+    e.preventDefault()
+    // * Form event target elements shows all elements it has
+    // * You can destructure those elements by their ID (<input id='something' />)
+    const {username, password} = e.target.elements
+
+    onSubmit({
+      username: username.value,
+      password: password.value,
+    })
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+    </form>
+  )
+}
+
 const App = () => {
   const [openModal, setOpenModal] = useState('none')
 
   const handleModalClose = () => setOpenModal('none')
+
+  // * FormData object: https://developer.mozilla.org/ko/docs/Web/API/FormData/FormData
+  const login = formData => console.log('login', formData)
+  const register = formData => console.log('register', formData)
 
   return (
     <>
@@ -26,6 +59,7 @@ const App = () => {
       >
         <button onClick={handleModalClose}>close</button>
         <h3>Login</h3>
+        <LoginForm onSubmit={login} buttonText="Login" />
       </Dialog>
       <Dialog
         aria-label="Registration form"
@@ -34,6 +68,7 @@ const App = () => {
       >
         <button onClick={handleModalClose}>close</button>
         <h3>Register</h3>
+        <LoginForm onSubmit={register} buttonText="Register" />
       </Dialog>
     </>
   )
