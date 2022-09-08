168d597136e2a17980ff112f5f0bc16e3119b0ef
etczrn <etczrn@gmail.com>
2022-09-08 15:35:04 +0900

    Use dialog #27

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,34 +1,44 @@
-// 🐨 you'll need to import react and createRoot from react-dom up here
-import React from 'react'
+import '@reach/dialog/styles.css'
+import React, {useState} from 'react'
 import {createRoot} from 'react-dom/client'
-
-// 🐨 you'll also need to import the Logo component from './components/logo'
+import Dialog from '@reach/dialog'
 import {Logo} from 'components/logo'
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
 const App = () => {
-  const handleBtnClick = e => {
-    const btnName = e.target.textContent
-    alert(`${btnName} clicked!`)
-  }
+  const [openModal, setOpenModal] = useState('none')
+
+  const handleModalClose = () => setOpenModal('none')
 
   return (
     <>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={handleBtnClick}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={handleBtnClick}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog
+        aria-label="Login form"
+        isOpen={openModal === 'login'}
+        onDismiss={handleModalClose}
+      >
+        <button onClick={handleModalClose}>close</button>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog
+        aria-label="Registration form"
+        isOpen={openModal === 'register'}
+        onDismiss={handleModalClose}
+      >
+        <button onClick={handleModalClose}>close</button>
+        <h3>Register</h3>
+      </Dialog>
     </>
   )
 }
 
-// 🐨 use createRoot to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
 export {root}
