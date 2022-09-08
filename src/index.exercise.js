da877a904986e5e4e88909918f69224af37e00a1
etczrn <etczrn@gmail.com>
2022-09-08 14:15:55 +0900

    Render App component #27

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,34 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
+import React from 'react'
+import {createRoot} from 'react-dom/client'
 
 // 🐨 you'll also need to import the Logo component from './components/logo'
+import {Logo} from 'components/logo'
 
 // 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
 // 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+const App = () => {
+  const handleBtnClick = e => {
+    const btnName = e.target.textContent
+    alert(`${btnName} clicked!`)
+  }
+
+  return (
+    <>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={handleBtnClick}>Login</button>
+      </div>
+      <div>
+        <button onClick={handleBtnClick}>Register</button>
+      </div>
+    </>
+  )
+}
 
 // 🐨 use createRoot to render the <App /> to the root element
 // 💰 find the root element with: document.getElementById('root')
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+export {root}
