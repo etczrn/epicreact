fcd0b3334d441565d0aea1e002124f5932eaff41
etczrn <etczrn@gmail.com>
2022-09-09 15:28:20 +0900

    Wire up authentication #30

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,25 +1,36 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
 
-import * as React from 'react'
+import {useState} from 'react'
 // 🐨 you're going to need this:
-// import * as auth from 'auth-provider'
+import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
 function App() {
   // 🐨 useState for the user
+  const [user, setUser] = useState(null)
 
   // 🐨 create a login function that calls auth.login then sets the user
   // 💰 const login = form => auth.login(form).then(u => setUser(u))
+  const login = form => auth.login(form).then(user => setUser(user))
+
   // 🐨 create a registration function that does the same as login except for register
+  const register = form => auth.register(form).then(user => setUser(user))
 
   // 🐨 create a logout function that calls auth.logout() and sets the user to null
+  const logout = () => {
+    auth.logout()
+    setUser(null)
+  }
 
   // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
   // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-
-  return <UnauthenticatedApp />
+  return user ? (
+    <AuthenticatedApp user={user} logout={logout} />
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
+  )
 }
 
 export {App}
