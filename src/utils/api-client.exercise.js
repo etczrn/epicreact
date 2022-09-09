dd1f2a87982c8b03f8248328098da2a26eaf2422
etczrn <etczrn@gmail.com>
2022-09-09 16:45:01 +0900

    Logout automatically on 401 #30

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,3 +1,4 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(
@@ -15,6 +16,14 @@ function client(
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     const data = await response.json()
+
+    if (response.status === 401) {
+      await auth.logout()
+      window.location.assign(window.location)
+
+      return Promise.reject({message: 'Please re-authenticate'})
+    }
+
     if (response.ok) {
       return data
     } else {
