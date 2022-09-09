a0ad36f3a8039a1341cc26b40b58e37c64e3c8b6
etczrn <etczrn@gmail.com>
2022-09-09 16:17:51 +0900

    Get user data on load #30

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,31 +1,40 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
 
-import {useState} from 'react'
-// 🐨 you're going to need this:
+import {useState, useEffect} from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from 'utils/api-client.exercise'
+
+async function getUser() {
+  let user = null
+  const token = await auth.getToken()
+
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
-  // 🐨 useState for the user
   const [user, setUser] = useState(null)
 
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
+  useEffect(() => {
+    getUser().then(user => setUser(user))
+  }, [])
+
   const login = form => auth.login(form).then(user => setUser(user))
 
-  // 🐨 create a registration function that does the same as login except for register
   const register = form => auth.register(form).then(user => setUser(user))
 
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
   const logout = () => {
     auth.logout()
     setUser(null)
   }
 
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
   return user ? (
     <AuthenticatedApp user={user} logout={logout} />
   ) : (
