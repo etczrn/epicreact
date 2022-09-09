3bb6ed5fa046ec2c7a7edd704def0985cbad7f26
etczrn <etczrn@gmail.com>
2022-09-09 16:52:13 +0900

    Support posting data #30

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -3,12 +3,14 @@ const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {data, token, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
-    method: 'GET',
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
